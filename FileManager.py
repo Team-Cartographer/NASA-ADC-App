a42d651c8a2eabdaf8671f32f01cd4735010fde8
abhi arya <abhia9167@gmail.com>
2023-03-13 22:42:25 -0700

    Moved things around to prepare for jsons in the future.

--- FileManager.py
+++ FileManager.py
@@ -8,7 +8,14 @@ from shutil import move
 from dotenv import load_dotenv, set_key
 from utils import show_error, show_info
 from csv import reader
+import json
+from time import localtime, strftime
 
+class Save:
+    def __init__(self, json_path, folder_path):
+        self.json_path = json_path
+        self.folder_path = folder_path
+        print(f'Saved')
 
 # Setting up '.env' created by PathFetcher
 if not os.path.exists(os.getcwd() + '/.env'):
@@ -69,6 +76,34 @@ def get_min_y() -> int:
 def get_lunar_rad() -> float:
     return float(os.getenv('LUNAR_RAD'))
 
+'''
+def load_save(json_path : str) -> dict:
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def write_save(latitude_path : str, longitude_path : str,
+               height_path : str, slope_path : str, dist : str,
+               size_constant : str , player_pos : str) -> str:
+    data : dict = {
+        "LATITUDE_PATH": latitude_path,
+        "LONGITUDE_PATH": longitude_path,
+        "HEIGHT_PATH": height_path,
+        "SLOPE_PATH": slope_path,
+
+        "DIST_BETWEEN_POINTS": dist,
+        "SIZE_CONSTANT": size_constant,
+        "PLAYER_POSITION": player_pos
+    }
+
+    date : str = str(strftime("%Y-%m-%d %H:%M:%S", localtime())).replace(":", "-")
+    jsonpath : str = os.path.join(os.getcwd(), f'{date}.json')
+    with open(jsonpath, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    return jsonpath
+'''
+
 
 # IMPORTANT PATHING
 parent_path: str = os.getcwd()
@@ -93,3 +128,5 @@ if __name__ == '__main__':
         # If Directories exist, Notify User.
         show_info('ADC App Installation Update',
                   "Folder Already Exists on " + parent_path + '\nFiles have been updated.')
+
+
