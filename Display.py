d2d13280811551413cc1ca066b875846c967a56c
abhi arya <abhia9167@gmail.com>
2023-03-21 22:16:31 -0700

    Implemented more soundtracks and volume controller. Sound selection coming soon.

--- Display.py
+++ Display.py
@@ -4,6 +4,7 @@ from utils import get_azi_elev, \
     latitude_from_rect, longitude_from_rect, \
     height_from_rect, slope_from_rect, show_error
 from ursina.prefabs.first_person_controller import FirstPersonController
+from random import choice 
 from ursina.application import quit # USE THIS, NOT PYTHON quit()
 
 # Window Declarations and Formatting -------------
@@ -18,6 +19,7 @@ SIZE_CONSTANT = fm.get_size_constant()
 EDITOR_SCALE_FACTOR = 3
 PLAYER_SCALE_FACTOR = 10
 RESET_LOC = (0, 400, 0)  # Default PLAYER Positional Value
+VOLUME = 0.15
 
 # Load the Data
 try:
@@ -137,6 +139,34 @@ vc = EditorCamera(enabled=False, zoom_speed=2) # Note: THIS MUST BE INITIALIZED 
 player = FirstPersonController(position=RESET_LOC, speed=500, mouse_sensitivity=Vec2(25, 25), enabled=False, gravity=False)
 player.cursor.scale = 0.00000000001 # Hides the Cursor from the App Display
 
+track_list = ['Audio/track_1.mp3', 'Audio/track_2.mp3', 'Audio/track_3.mp3', 'Audio/bouyer_lonely_wasteland.mp3'
+              , 'Audio/bouyer_sonata_csharp_minor.mp3', 'pause_track.mp3']
+run_music = Audio(
+    # TODO Make a Tracklist Randomizer
+    track_list[2], # Change this for different tracks.
+    # TODO Make a Track Picker and Volume Control
+    volume=VOLUME,
+    loop=True,
+)
+run_music.stop(destroy=False)
+
+pause_music = Audio(
+    track_list[5],
+    volume=VOLUME,
+    loop=True
+)
+pause_music.stop(destroy=False)
+
+start_menu_music = Audio(
+    track_list[3],
+    volume=VOLUME,
+    loop=True
+)
+
+def play_run_music():
+    #run_music.clip(choice(track_list))
+    run_music.play()
+
 
 # Input Functions and Toggles -------------
 def input(key):
@@ -186,7 +216,7 @@ def input(key):
         exit(0)
 
     # Pause
-    if key == 'escape' and pause_button.enabled is False:
+    if key == 'escape' and pause_button.enabled is False and volume_slider.enabled is False:
         t_lat.disable()
         t_lon.disable()
         t_ht.disable()
@@ -207,11 +237,19 @@ def input(key):
         t_quit.enable()
         color_key.disable()
         return_button.enable()
+        pause_music.play()
+        run_music.stop(destroy=False)
 
 
 # Game Loop Update() Functions -------------
 height_vals = ground_player.model.height_values
 def update():
+    # Audio Update
+    VOLUME = volume_change()
+    pause_music.volume = VOLUME
+    start_menu_music.volume = VOLUME
+    run_music.volume = VOLUME
+
     # Map Failsafe
     bound = SIZE_CONSTANT*10/2 - 200
     if -bound > player.position.x or player.position.x > bound or -bound > player.position.z or player.position.z > bound:
@@ -266,6 +304,8 @@ def update():
     #if hit_x.hit:
     #    print(f'X: {hit_x}')
 
+    print(volume_slider.value)
+
 
 
 # Create Start Menu -------------
@@ -288,6 +328,9 @@ def start_game():
     load_button.disable()
     launch_button.disable()
     t_current_site.disable()
+    volume_slider.disable()
+    play_run_music()
+    pause_music.stop(destroy=False)
 
 
 # Unpause Button Function -------------
@@ -312,6 +355,8 @@ def on_unpause():
     mini_dot.enable()
     t_pos.enable()
     return_button.disable()
+    pause_music.stop(destroy=False)
+    play_run_music()
 
 def load_button_init():
     import tkinter as tk
@@ -327,20 +372,38 @@ def load_button_init():
 
 # Start Menu Text and Buttons -------------
 t_start_menu = Text(text="Welcome to Team Cartographer's 2023 NASA ADC Application", x=-0.35, y=0.08)
-t_start_menu_creds = Text(text="https://github.com/abhi-arya1/NASA-ADC-App \n \n      https://github.com/pokepetter/ursina", x=-0.275, y=-0.07, color=color.dark_gray)
-def main_menu_init():
+t_start_menu_creds = Text(text="https://github.com/abhi-arya1/NASA-ADC-App \n \n      https://github.com/pokepetter/ursina", x=-0.275, y=-0.135, color=color.dark_gray)
+
+start_button = Button(text='Main Menu', color=color.gray, highlight_color=color.dark_gray, scale=(0.2, 0.05), y=-0.01)
+def main_menu_returner():
     t_start_menu.disable(), t_start_menu_creds.disable(), start_button.disable()
+    creds_button.disable()
+    start_menu_music.stop(destroy=False)
     t_current_site.enable()
     launch_button.enable()
     load_button.enable()
+    pause_button.disable()
+    t_quit.disable()
+    t_pause.disable()
+    return_button.disable()
+    pause_music.play()
+    volume_slider.enable()
 
-start_button = Button(text='Main Menu', color=color.gray, highlight_color=color.dark_gray, scale=(0.2, 0.05), y=-0.01)
-start_button.on_click = main_menu_init
+start_button.on_click = main_menu_returner
+
+creds_button = Button(text='Credits', color=color.gray, highlight_color=color.dark_gray, scale=(0.2, 0.05), y=-0.07)
+def creds_init():
+    # TODO: ADD PROPER CREDITS
+    pass
 
 # For Main Menu
+def volume_change():
+    return volume_slider.value
+
 t_current_site = Text(text=f"Currently Visiting: Shackleton", x=-0.2, y=0.1, scale=1.25, enabled=False)
 launch_button = Button(text="Visualize Site",  color=color.gray, highlight_color=color.dark_gray, scale=(0.25, 0.06), x=0, y=0.0, enabled=False)
 load_button = Button(text="Load A Site", color=color.dark_gray, highlight_color=Color(0.15, 0.15, 0.15, 1.0), scale=(0.25, 0.06), x=0, y=-0.08, enabled=False)
+volume_slider = ThinSlider(text='Volume', value=0.15, dynamic=True, on_value_changed=volume_change, enabled=False, x=-0.2, y=-0.2)
 
 launch_button.on_click = start_game
 load_button.on_click = load_button_init
@@ -349,29 +412,11 @@ load_button.on_click = load_button_init
 # Pause Menu Text and Buttons -------------
 t_pause = Text(text="You are Currently Paused...", x=-0.16, y=0.08, enabled=False)
 pause_button = Button(text='Click to Unpause', color=color.gray, highlight_color=color.dark_gray, scale=(0.23, 0.05), enabled=False)
-t_quit = Text(text="Press 'LShift+Q' to quit.", x=-0.14, y=-0.14, enabled=False)
+t_quit = Text(text="Press 'LShift+Q' to quit.", x=-0.14, y=-0.17, enabled=False)
 pause_button.on_click = on_unpause
 
 return_button = Button(text='Main Menu', color=color.gray, highlight_color=color.dark_gray, scale=(0.23, 0.06), enabled=False, x=0, y=-0.06)
-def main_menu_returner():
-    t_current_site.enable()
-    launch_button.enable()
-    load_button.enable()
-    pause_button.disable()
-    t_quit.disable()
-    t_pause.disable()
-    return_button.disable()
-
 return_button.on_click = main_menu_returner
-
-track_list = ['Audio/track_1.mp3', 'Audio/track_2.mp3', 'Audio/track_3.mp3']
-music = Audio(
-    # TODO Make a Tracklist Randomizer
-    track_list[2], # Change this for different tracks.
-    # TODO Make a Track Picker and Volume Control
-    volume=0.15,
-    loop=True,
-)
 
 # Runs Display.py -------------
 if __name__ == '__main__':
