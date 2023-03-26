3b569b2b7f5637c06b8cfbda10200a9f905974c8
Efe Celik <suleymanefeclk2@gmail.com>
2023-03-25 21:28:31 -0700

    BOOOMMM

--- DataProcessor.py
+++ DataProcessor.py
@@ -3,6 +3,10 @@ from utils import timeit
 import FileManager as fm
 from Constants import LUNAR_RADIUS, EARTH_X, EARTH_Y, EARTH_Z
 import time
+from tqdm import tqdm
+from utils import push_to_json, load_json
+
+data = load_json(fm.INFO_JSONPATH)
 
 LUNAR_RADIUS_METERS = (LUNAR_RADIUS * 1000)
 EARTH_X_METERS = (EARTH_Y * 1000)
@@ -26,24 +30,21 @@ longitude_list = np.loadtxt(fm.get_longitude_file_path(), delimiter=',', dtype=f
 height_list = np.loadtxt(fm.get_height_file_path(), delimiter=',', dtype=float)
 slope_list = np.loadtxt(fm.get_slope_file_path(), delimiter=',', dtype=float)
 
-print(time.time()- startt_time)
+print(time.time() - startt_time)
 
 @timeit
 def process_data():
     start_time = time.time()
-    # Convert latitudes and longitudes to radians
+
     latitude_radians = np.radians(latitude_list)
     longitude_radians = np.radians(longitude_list)
 
-    # Calculate the radius
     radius = LUNAR_RADIUS_METERS + height_list
 
-    # Calculate x, y, and z coordinates
     x = radius * np.cos(latitude_radians) * np.cos(longitude_radians)
     y = radius * np.cos(latitude_radians) * np.sin(longitude_radians)
     z = radius * np.sin(latitude_radians)
 
-    # Calculate elevation and azimuth
     dx, dy, dz = EARTH_X_METERS - x, EARTH_Y_METERS - y, EARTH_Z_METERS - z
     r = np.sqrt(dx ** 2 + dy ** 2 + dz ** 2)
     rz = dx * np.cos(latitude_radians) * np.cos(longitude_radians) + dy * np.cos(latitude_radians) * np.sin(longitude_radians) + dz * np.sin(latitude_radians)
@@ -53,42 +54,35 @@ def process_data():
     c2 = (np.cos(latitude_radians) * np.sin(EARTH_LAT)) - (np.sin(latitude_radians) * np.cos(EARTH_LAT) * np.cos(EARTH_LONG - longitude_radians))
     azimuth = np.degrees(np.arctan2(c1, c2))
 
-    # Create a_star_data_array
-    a_star_data_array = np.column_stack((x.flatten(), y.flatten(), z.flatten(), slope_list.flatten(), azimuth.flatten(), elevation.flatten(), np.degrees(latitude_list).flatten(), np.degrees(longitude_list).flatten(), height_list.flatten()))
+    a_star_data_array = np.column_stack((x.flatten(), y.flatten(), z.flatten(), slope_list.flatten(), azimuth.flatten(), elevation.flatten(), latitude_list.flatten(), longitude_list.flatten(), height_list.flatten()))
     print(time.time() - start_time)
-    print(type(a_star_data_array))
-    print(a_star_data_array[0])
-    print(a_star_data_array[0][0])
-    # Save a_star_data_array to CSV file
-    # np.savetxt('AStarRawData.csv', a_star_data_array, delimiter=',', header='x,y,z,slope,azimuth,elevation,latitude,longitude,height', comments='')
 
+    min_x: float = np.min(a_star_data_array[:, 0])
+    min_y: float = np.min(a_star_data_array[:, 1])
+    min_z: float = np.min(a_star_data_array[:, 2])
+    max_height: float = np.max(a_star_data_array[:, 8])
 
+    # TODO: Add max x, max y, and  max z to data
+    data["MAX_HEIGHT"] = max_height
 
-    # adj_array: list = []
-    # for i in tqdm(range(len(a_star_data_array)), desc="Creating AStar Data Array"):
-    #     # x[0], y[1], z(height)[2], slope[3], azi[4], elev[5], lat[6], long[7], height[8]
-    #     tmp: list = [int(a_star_data_array[i][0]), int(a_star_data_array[i][1]),
-    #                  int(a_star_data_array[i][2]),
-    #                  a_star_data_array[i][3], a_star_data_array[i][4], a_star_data_array[i][5], a_star_data_array[i][6],
-    #                  a_star_data_array[i][7], a_star_data_array[i][8]]
-    #     adj_array.append(tmp)
-    #
-    # sorted_array = sorted(adj_array, key=lambda arr: arr[1])
-    #
-    # array_to_be_written: list = [[] for _ in range(SIZE)]
-    #
-    # for i in range(len(sorted_array)):
-    #     array_to_be_written[i // SIZE].append(sorted_array[i])
-    #
-    # for i in range(len(array_to_be_written)):
-    #     array_to_be_written[i]: list = sorted(array_to_be_written[i], key=lambda arr: arr[0])
-    #
-    # for i in range(len(array_to_be_written)):
-    #     for j in range(len(array_to_be_written[0])):
-    #         array_to_be_written[j][i][0] = i
-    #         array_to_be_written[j][i][1] = j
-    #
-    # push_to_json(fm.ASTAR_JSONPATH, array_to_be_written)
+    a_star_data_array[:, 0] += abs(min_x)
+    a_star_data_array[:, 1] += abs(min_y)
+    a_star_data_array[:, 2] += abs(min_z)
+
+    a_star_data_array = a_star_data_array[a_star_data_array[:, 1].argsort()]
+
+    a_star_data_array = a_star_data_array.tolist()
+
+    array_to_be_written: list = [[] for _ in range(SIZE)]
+
+    for i in range(len(a_star_data_array)):
+        array_to_be_written[i // SIZE].append(a_star_data_array[i])
+
+    for i in range(len(array_to_be_written)):
+        array_to_be_written[i]: list = sorted(array_to_be_written[i], key=lambda arr: arr[0])
+
+    push_to_json(fm.ASTAR_JSONPATH, array_to_be_written)
+    push_to_json(fm.INFO_JSONPATH, data)
 
 
 if __name__ == "__main__":
